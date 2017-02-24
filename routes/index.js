e95b9adf3d5eb45a649d7528a52031c4752ca35a
adamsaparudin <adamsaparudin@gmail.com>
2017-02-24 11:33:51 +0700

    API SUCCESS

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+let searchYoutube = require('youtube-search');
+
+let YoutubeOpts = {
+  maxResults: 5,
+  key: 'AIzaSyAsdJlWuuDWMrPm4seP4D7y1lUC5FMpXUQ'
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('pages/index', { title: 'Express' });
+});
+
+router.get('/search/:keyword', function(req, res, next) {
+  return new Promise(function(res, rej) {
+    searchYoutube(req.params.keyword, YoutubeOpts, function(err, results) {
+      if (err) {
+        rej(err)
+      }
+      else {
+        res(results)
+      }
+    })
+  }).then(function(results) {
+    res.render('pages/search-result', {results: results})
+  })
 });
 
 module.exports = router;
